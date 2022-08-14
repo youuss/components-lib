da63df74bd94b660e8e7f3666480623824a6f2f0
youus <alecshen0926@foxmail.com>
2022-08-14 22:23:31 +0800

    🎉 init

---
+++ props/HelloWorldProps.js
@@ -0,0 +1,20 @@
+/**
+ * @Description
+ * @Author youus
+ * @Date 2022/8/14 21:54
+ * @Version v1.0.0
+ *
+ * Hello, humor
+ */
+const helloWorldProps = {
+    text: {
+        type: String,
+        default: 'Hello, humor'
+    },
+    name: {
+        type: String,
+        default: 'youus'
+    }
+}
+
+export default helloWorldProps;
---
+++ vue2-lib/lib/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    {{ name }} - {{ text }}
+  </div>
+</template>
+<script>
+import helloWorldProps from "../../../props/HelloWorldProps";
+export default {
+  name: 'HelloWorld',
+  props: {
+    ...helloWorldProps
+  }
+}
+</script>
